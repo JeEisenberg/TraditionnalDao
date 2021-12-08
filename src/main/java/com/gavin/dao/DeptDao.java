beb3df98726ace009aeadc254b83d5748b2e512f
杰森伯格 <94214876+JeEisenberg@users.noreply.github.com>
2021-12-08 13:21:10 +0800

    mapper方式开发

--- src/main/java/com/gavin/dao/DeptDao.java
+++ src/main/java/com/gavin/dao/DeptDao.java
@@ -2,9 +2,12 @@ package com.gavin.dao;
 
 import com.gavin.pojo.Dept;
 import com.gavin.pojo.Emp;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface DeptDao {
 
-
+@Transactional(propagation = Propagation.REQUIRED ,isolation = Isolation.DEFAULT)
    Dept findEmpByDeptno(Integer deptno);
 }
